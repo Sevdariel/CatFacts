181bf4360d2e9302f3639fc8e21f7f95364b45d4
Michał Zalewski <michal.zalewski1995@gmail.com>
2024-03-02 18:06:18 +0100

    Add formGroup to login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  public loginForm = new FormGroup({
+    login: new FormControl(),
+    password: new FormControl(),
+  });
 
+  constructor() {
+
+  }
 }
