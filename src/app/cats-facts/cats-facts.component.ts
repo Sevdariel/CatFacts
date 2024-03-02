789bb11bc69a1f066218558a776bcc1da9fe7294
Michał Zalewski <michal.zalewski1995@gmail.com>
2024-03-02 13:12:26 +0100

    Add cats facts component

---
+++ src/app/cats-facts/cats-facts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cats-facts',
+  standalone: true,
+  imports: [],
+  templateUrl: './cats-facts.component.html',
+  styleUrl: './cats-facts.component.scss'
+})
+export class CatsFactsComponent {
+
+}
