b378841b1725b62deee4a758ccdb331dc7b15956
Michał Zalewski <michal.zalewski1995@gmail.com>
2024-03-02 13:14:01 +0100

    Add routes to login and cats fact components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { CatsFactsComponent } from './cats-facts/cats-facts.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routesConfig: Routes = [
+    {
+        path: '',
+        component: LoginComponent,
+        title: 'Login page',
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        title: 'Login page',
+    },
+    {
+        path: 'facts',
+        component: CatsFactsComponent,
+        title: 'Cats facts',
+    },
+];
