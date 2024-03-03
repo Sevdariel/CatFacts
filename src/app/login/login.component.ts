28cd38f1cecf29777450f4f25735b96c1dad6233
Michał Zalewski <michal.zalewski1995@gmail.com>
2024-03-03 17:00:38 +0100

    Add login form validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LoginService } from '../services/login.service';
 import { Router } from '@angular/router';
+import { ILoginForm } from '../model/cat-facts.model';
 
 @Component({
   selector: 'app-login',
@@ -14,19 +15,35 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
-  public loginForm = new FormGroup({
-    login: new FormControl(),
-    password: new FormControl(),
-  });
+export class LoginComponent implements OnInit {
+
+  @HostListener('submit')
+  public submitListener() {
+    if (!this.loginForm.valid) {
+      this.loginFormValidationAfterSubmit = true;
+    } else { this.loginFormValidationAfterSubmit = false }
+  }
+
+  public loginForm: FormGroup;
+  public loginFormValidationAfterSubmit: boolean = false;
 
   constructor(
     private loginService: LoginService,
     private router: Router,
+    private formBuilder: FormBuilder,
   ) { }
 
+  public ngOnInit(): void {
+    this.loginForm = this.formBuilder.nonNullable.group<ILoginForm>({
+      login: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
   public login() {
-    this.loginService.login();
-    this.router.navigateByUrl('/facts')
+    if (this.loginForm.valid) {
+      this.loginService.login();
+      this.router.navigateByUrl('/facts')
+    }
   }
 }
--- src/app/model/cat-facts.model.ts
+++ src/app/model/cat-facts.model.ts
@@ -1,3 +1,10 @@
+import { FormControl } from '@angular/forms';
+
 export interface ICatFact {
     data: Array<string>;
+}
+
+export interface ILoginForm {
+    login: FormControl;
+    password: FormControl;
 }
